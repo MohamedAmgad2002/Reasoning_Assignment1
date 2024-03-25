4fb1e5a101a9e0e8b680553388a36d3c825ea6dd
Yousef AlZayat <67283820+YousefAlzayat@users.noreply.github.com>
2024-03-25 15:15:38 +0200

    More small advancements

--- Parse.py
+++ Parse.py
@@ -19,17 +19,63 @@ def getBracketEndIndex(word: str):
             return i+1
     return -1
 
-def parseString(word: str):
-    if word.startswith("ForAll("):
-        ForAll(word[7:word.find(',')], word[word.find(',')+1:word.find(',')+2+getBracketEndIndex(word[word.find(',')+2:])])
-    elif word.startswith("Exists("):
-        Exists(word[7:word.find(',')], word[word.find(',')+1:word.find(',')+2+getBracketEndIndex(word[word.find(',')+2:])])
+def bracketCount(word: str, limit: int):
+    count = 0
+    if limit > len(word):
+        raise Exception("Limit can't be bigger than word length")
+    for i in range(limit):
+        if word[i] == '(':
+            count += 1
+        elif word[i]==')':
+            count -= 1
+        
+    return count
 
-    
+def getFirstInstance(word: str, symbol: str):
+    i = 0
+    while i < len(word):
+        result = word.find(symbol, i)
+        if result != -1 and bracketCount(word, result) == 0:
+            return result
+        
+        i += 1
+    return -1
+
+def splitStatement(word: str):
+    # if word.startswith("ForAll("):
+        # ForAll(word[7:word.find(',')], word[word.find(',')+1:word.find(',')+2+getBracketEndIndex(word[word.find(',')+2:])])
+    # elif word.startswith("Exists("):
+        # Exists(word[7:word.find(',')], word[word.find(',')+1:word.find(',')+2+getBracketEndIndex(word[word.find(',')+2:])])
+
+    # for i in range(len(word)):
+        # if bracketCount(word, i) == 0:
+        #     print(word[i], end='')
+    # print(word, end='-\n')
+    print(word)
+    bicond = getFirstInstance(word, '<=>')
+    if bicond != -1:
+        splitStatement(word[:bicond-1])
+        splitStatement(word[bicond+4:])
+        return
+    cond = getFirstInstance(word, '=>')
+    if cond != -1:
+        splitStatement(word[:cond-1])
+        splitStatement(word[cond+3:])
+        return
+    disjunct = getFirstInstance(word, '|')
+    if disjunct != -1:
+        splitStatement(word[:disjunct-1])
+        splitStatement(word[disjunct+2:])
+        return
+    # elif bicond 
+    # print(getFirstInstance(word, '=>'))
+    # print(getFirstInstance(word, '&'))
 
 
 
-parseString("Exists(y,(y & x)) | (x & z)")
+print("Exists(y,(y & x)) | (x & z) => (s | z) <=> ForAll(x, (s & z)) | (s & y | z) => s")
+print("01234567890123456789012345678901234567890123456789012345678901234567890123456789")
+splitStatement("Exists(y,(y & x)) | (x & z) => (s | z) <=> ForAll(x, (s & z)) | (s & y | z) => s")
 
 """
 Ax. ()
@@ -37,6 +83,6 @@ ForAll(y,(y & x)) | (x & z)
 Exists(y,(y & x)) | (x & z)
 (Exists(y,(y <-> x)) | (x & z)) <-> (x)
 (Exists(y,(y & x)) | (x & z)) -> (x)
-(1 & z)
-[(~y | x), (~x | z), (z | ~y)]
+(x & z)
+l = ["(~y | x)", "(~x | z)", "(z | ~y)"]
 """
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from Exists import Exists
 from ForAll import ForAll
 
-# Takes a statement that looks like the following: smth -> y
+# Takes a statement that looks like the following: (smth) -> (y)
 def eliminate_implication(word: str):
-    for i in range(len(word)):
+    for i in range(len(word)-1):
         if word[i:i+2] == '->':
             word = '~' + word[:i] + '|' + word[i+2:]
 
@@ -22,10 +22,17 @@ def move_negation_inward(word: str):
     # print('(', end='')
     result = ''
     for j in comp:
+        print(j, end='-\n')
         if j == '~|':
             result += '&'
         elif j == '~&':
             result += '|'
+        elif j.startswith('~Exists('):
+            result += " ForAll"
+            result += j[7:]
+        elif j.startswith('~ForAll('):
+            result += " Exists"
+            result += j[7:]
         else:
             result += f' {j} '
     result = '(' + result[1:len(result)-1] + ')'
@@ -37,7 +44,8 @@ def remove_double_negation(word: str):
     return result
 
 # print(eliminate_implication("(~X) -> (Y)"))
-# print(move_negation_inward("(~~X | Y)"))
+print(move_negation_inward("(~~X | Y)"))
+print(move_negation_inward("~(Exists(x, q(x,y,z)))"))
 # print(remove_double_negation("~~X | Y"))
 # e = Exists('z', "Exists(q(x,y,z))")
 # e.changeVariable('p(x,y)')
